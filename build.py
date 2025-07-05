bea5fd9b079d738d2343a562017bbbe7dd0345d5
morning-start <morning-start@foxmail.com>
2025-07-05 16:45:10 +0800

    ci(workflow): 更新发布工作流中的构建产物路径
    将发布工作流中的构建产物路径从 `./dist/*` 改为 `./export/*`，以匹配 build.py 中的修改。同时 build.py 新增了将构建产物打包为压缩文件的功能，并输出到 export 目录。

--- build.py
+++ build.py
@@ -1,7 +1,30 @@
+# 将 dist/*打包为一个压缩包文件，根据系统类型，后缀名不同
+import platform
 import subprocess
+import tarfile
+import zipfile
+from pathlib import Path
+
+
+def compress_files(files: list[Path], output: Path):
+    """
+    压缩文件为一个压缩包文件
+    :param files: 要压缩的文件列表
+    :param output: 压缩包文件路径
+    """
+    if platform.system() == "Windows":
+        with zipfile.ZipFile(output, "w", zipfile.ZIP_DEFLATED) as zipf:
+            for file in files:
+                zipf.write(file, file.name)
+    else:
+        with tarfile.open(output, "w:gz") as tar:
+            for file in files:
+                tar.add(file, file.name)
+
 
 APP_NAME = "jvm"
 ICON = "logo.ico"
+VERSION = "1.0.0"
 # 构建 pyinstaller 命令
 cmd = [
     "pyinstaller",
@@ -15,5 +38,14 @@ try:
     # 执行 pyinstaller 命令
     subprocess.run(cmd, check=True)
     print("应用构建成功！")
+    # 压缩 dist 目录下的文件为一个压缩包文件
+    dist_dir = Path("dist")
+    export = Path("export")
+    export.mkdir(exist_ok=True)
+    platform_name = platform.system()
+    compress_files(
+        dist_dir.glob("*"),
+        export / f"{APP_NAME}-{platform_name.lower()}-{VERSION}.zip",
+    )
 except subprocess.CalledProcessError as e:
     print(f"应用构建失败: {e}")
